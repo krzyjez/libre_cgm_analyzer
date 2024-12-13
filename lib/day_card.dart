50404af2209c8083dc6b18358000246d87770303
Krzysztof <krzyjez@gmail.com>
2024-12-13 09:59:53 +0100

    są już komentarze w karcie

--- lib/day_card.dart
+++ lib/day_card.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'csv_parser.dart';
 
 class DayCardBuilder {
-  static Widget buildDayCard(dynamic day) {
+  /// Buduje widżet karty dla danego dnia.
+  ///
+  /// Zawiera nagłówek z datą i obszar roboczy z trzema wierszami.
+  static Widget buildDayCard(Day day) {
     return Card(
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(12.0),
@@ -11,31 +15,131 @@ class DayCardBuilder {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Nagłówek
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(12.0),
-              topRight: Radius.circular(12.0),
-            ),
-            child: Container(
-              width: double.infinity,
-              color: Colors.grey[800],
-              padding: const EdgeInsets.all(8.0),
-              child: Text(
-                'Data: ${DateFormat('yyyy-MM-dd').format(day.date)}',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 8.0),
+          _buildHeader(day.date),
+          // Odstęp między nagłówkiem i obszarem roboczym
+          const SizedBox(height: 4.0),
           // Obszar roboczy
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text("Buba"),
+          _buildWorkingArea(day),
+        ],
+      ),
+    );
+  }
+
+  /// Buduje nagłówek dla karty dnia.
+  ///
+  /// Zawiera datę z zaokrąglonymi rogami i ciemnozielonym tłem.
+  static Widget _buildHeader(DateTime date) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(12.0),
+        topRight: Radius.circular(12.0),
+      ),
+      child: Container(
+        width: double.infinity,
+        color: Colors.green[900],
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          'Data: ${DateFormat('yyyy-MM-dd').format(date)}',
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  /// Buduje obszar roboczy dla karty dnia.
+  /// Zawiera trzy wiersze z tekstem.
+  static Widget _buildWorkingArea(Day day) {
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Komentarze dzienne
+          _buildDailyComments(day),
+          // wykres i statystyki
+          _buildChartAndStats(day),
+          // notatki
+          _buildNotes(day),
+        ],
+      ),
+    );
+  }
+
+  /// Buduje sekcję komentarzy dziennych.
+  static Widget _buildDailyComments(Day day) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      color: Colors.blue[100], // Kolor tła dla pierwszego kontenera
+      child: const Text('wiersz 1'),
+    );
+  }
+
+  /// Buduje sekcję wykresu i statystyk.
+  static Widget _buildChartAndStats(Day day) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      color: Colors.red[100], // Kolor tła dla drugiego kontenera
+      child: Row(
+        children: [
+          Container(
+            color: Colors.green[100],
+            padding: const EdgeInsets.all(8.0),
+            child: const Text('wykres'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            child: const Text('statystyka'),
           ),
         ],
+      ),
+    );
+  }
+
+  /// Buduje sekcję notatek.
+  static Widget _buildNotes(Day day) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      color: Colors.yellow[100], // Kolor tła dla trzeciego kontenera
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: day.notes
+            .map((note) => Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: RichText(
+                    text: TextSpan(
+                      children: [
+                        _createTextSpan(
+                            '${DateFormat('HH:mm').format(note.timestamp)} ',
+                            fontWeight: FontWeight.bold),
+                        _createTextSpan(note.note),
+                      ],
+                    ),
+                  ),
+                ))
+            .toList(),
+      ),
+    );
+  }
+
+  /// Tworzy obiekt TextSpan z podanym tekstem i stylami.
+  ///
+  /// [text] - tekst do wyświetlenia.
+  /// [fontSize] - rozmiar czcionki, domyślnie 14.
+  /// [color] - kolor tekstu, domyślnie czarny.
+  /// [fontWeight] - grubość czcionki, domyślnie normalna.
+  static TextSpan _createTextSpan(String text,
+      {double fontSize = 14,
+      Color color = Colors.black,
+      FontWeight fontWeight = FontWeight.normal}) {
+    return TextSpan(
+      text: text,
+      style: TextStyle(
+        fontSize: fontSize,
+        color: color,
+        fontWeight: fontWeight,
       ),
     );
   }
