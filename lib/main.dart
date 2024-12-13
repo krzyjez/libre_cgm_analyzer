a9c2b183f1f35d2301552e81c160a5c292b47f75
Krzysztof <krzyjez@gmail.com>
2024-12-13 08:59:08 +0100

    ładne nagłówki na panelach

--- lib/main.dart
+++ lib/main.dart
@@ -122,35 +122,38 @@ class _MyHomePageState extends State<MyHomePage> {
                   itemBuilder: (context, index) {
                     final day = _csvParser.days[index];
                     return Card(
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            // Nagłówek
-                            ClipRRect(
-                              borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(12.0),
-                                topRight: Radius.circular(12.0),
-                              ),
-                              child: Container(
-                                width: double.infinity,
-                                color: Colors.grey[800],
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  'Data: ${DateFormat('yyyy-MM-dd').format(day.date)}',
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          // Nagłówek
+                          ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(12.0),
+                              topRight: Radius.circular(12.0),
+                            ),
+                            child: Container(
+                              width: double.infinity,
+                              color: Colors.grey[800],
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                'Data: ${DateFormat('yyyy-MM-dd').format(day.date)}',
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
                             ),
-                            const SizedBox(height: 8.0),
-                            // Obszar roboczy
-                            const Text("Buba"),
-                          ],
-                        ),
+                          ),
+                          const SizedBox(height: 8.0),
+                          // Obszar roboczy
+                          const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text("Buba"),
+                          ),
+                        ],
                       ),
                     );
                   },
